4bdb3d25a96d03dbaedc8027f43d3de301947b23
Preydet <pareydet@gmail.com>
2025-11-04 16:46:52 -0300

    C:/Program Files/Git/api/reservas funcionando

--- controllers/reservasController.js
+++ controllers/reservasController.js
@@ -1,17 +1,19 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 // Ruta al archivo JSON
 const reservasPath = path.join(__dirname, '../reservas.json');
 
 // Obtener todas las reservas
-const getReservas = (req, res) => {
-    fs.readFile(reservasPath, 'utf8', (err, data) => {
-        if (err) 
-            return res.status(500).json({ error: 'Error al leer las reservas' });
-        const reservas = JSON.parse(data) || '[]';
-        res.json(reservas);
-    });
+const getReservas = async(req, res) => {
+    try {
+        const data = await fs.readFile(reservasPath, 'utf8');
+        const reservas = JSON.parse(data) || [];
+        res.status(200).json(reservas);
+    } catch (err) {
+        console.error('Error al leer las reservas:', err);
+        res.status(500).json({ error: 'Error al leer las reservas' });
+    }  
 };
 
 module.exports = {
