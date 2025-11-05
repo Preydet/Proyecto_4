9fdb914133b16546458aa5418a43f126548f581c
Preydet <pareydet@gmail.com>
2025-11-05 12:09:38 -0300

    endpoint POST

--- controllers/reservasController.js
+++ controllers/reservasController.js
@@ -16,6 +16,38 @@ const getReservas = async(req, res) => {
     }  
 };
 
-module.exports = {
-    getReservas
-};
+// Crear una nueva reserva
+const createReserva = async(req, res) => {
+    try {
+        const data = await fs.readFile(reservasPath, 'utf8');
+        const reservas = JSON.parse(data) || [];
+
+        const nuevaReserva = {
+            id: reservas.length ? reservas[reservas.length - 1].id + 1 : 1,
+            hotel: req.body.hotel,
+            tipo_habitacion: req.body.tipo_habitacion,
+            num_huespedes: req.body.num_huespedes,
+            fecha_inicio: req.body.fecha_inicio,
+            fecha_fin: req.body.fecha_fin,
+            estado: req.body.estado || 'pendiente'
+        };
+
+//Validar campos obligatorios
+if (!nuevaReserva.hotel || !nuevaReserva.tipo_habitacion || !nuevaReserva.num_huespedes || !nuevaReserva.fecha_inicio || !nuevaReserva.fecha_fin) {
+    return res.status(400).json({ error: 'Faltan campos obligatorios' });
+}
+        reservas.push(nuevaReserva);
+        await fs.writeFile(reservasPath, JSON.stringify(reservas, null, 2));
+
+        res.status(201).json(nuevaReserva);
+    } catch (err) {
+        console.error('Error al crear la reserva:', err);
+        res.status(500).json({ error: 'Error al crear la reserva' });
+    }
+}
+
+    module.exports = {
+        getReservas,
+        createReserva
+    };
+
--- routers/reservasRouter.js
+++ routers/reservasRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getReservas } = require('../controllers/reservasController');
+const { getReservas, createReserva } = require('../controllers/reservasController');
 
 // Ruta para obtener todas las reservas
 router.get('/', getReservas);
+// Ruta para crear una nueva reserva
+router.post('/', createReserva);
 
 module.exports = router;
